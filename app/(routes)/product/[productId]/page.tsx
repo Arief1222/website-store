2f533c66189f6a5f54bb3dbc4854e04c8df1239b
Arief <miftahularifin102@gmail.com>
2025-10-24 17:51:15 +0700

    new params 2

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -5,35 +5,46 @@ import Info from "@/components/info";
 import ProductList from "@/components/product-list";
 import Container from "@/components/ui/container";
 
-interface ProductPageProps {
-    params: {
-        productId: string
-    };
-}
+type Params = { productId: string };
 
-const ProductPage: React.FC<ProductPageProps> = async ({params }) => {
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<Params>;
+}) {
+  const { productId } = await params;
 
-    const product = await getProduct(params.productId)
-    const suggestedProducts = await getProducts({
-        categoryId: product?.category?.id,
-    })
+  const product = await getProduct(productId);
+  const suggestedProducts = await getProducts({
+    categoryId: product?.category?.id,
+  });
+
+  if (!product) {
     return (
-        <div className="bg-white">
-            <Container>
-                <div className="px-4 py-10 sm:px-6 lg:px-8">
-                    <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-                        <Gallery images={product.images} />
-                        <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                            <Info data={product} />
-                        </div>
-                    </div>
-                    <hr className="my-10" />
-                    <ProductList title="Related Products" items={suggestedProducts} />
-                </div>
-
-            </Container>
-        </div>
+      <div className="bg-white">
+        <Container>
+          <div className="px-4 py-10 sm:px-6 lg:px-8">
+            <p className="text-sm text-foreground/70">Product not found.</p>
+          </div>
+        </Container>
+      </div>
     );
-}
+  }
 
-export default ProductPage;
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            <Gallery images={product.images ?? []} />
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <Info data={product} />
+            </div>
+          </div>
+          <hr className="my-10" />
+          <ProductList title="Related Products" items={suggestedProducts} />
+        </div>
+      </Container>
+    </div>
+  );
+}
