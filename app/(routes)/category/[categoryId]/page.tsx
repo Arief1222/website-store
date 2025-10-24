b46fc203b2b43def65f9ca200279a800723cadf4
Arief <miftahularifin102@gmail.com>
2025-10-24 17:40:58 +0700

    new params

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,44 +1,41 @@
-import getCategory from "@/actions/get-category"
-import getProducts from "@/actions/get-products"
-import Banner from "@/components/banner"
-import Container from "@/components/ui/container"
-import NoResults from "@/components/ui/no-result"
-import ProductCard from "@/components/ui/product-card"
+import getCategory from "@/actions/get-category";
+import getProducts from "@/actions/get-products";
+import Banner from "@/components/banner";
+import Container from "@/components/ui/container";
+import NoResults from "@/components/ui/no-result";
+import ProductCard from "@/components/ui/product-card";
 
-interface CategoryPageProps {
-    params: {
-        categoryId: string
-    }
-}
+type Params = { categoryId: string };
 
-const CategoryPage: React.FC<CategoryPageProps> = async ({
-    params
-}) => {
-    const products = await getProducts({
-        categoryId: params.categoryId
-    })
+export default async function CategoryPage({
+  params,
+}: {
+  params: Promise<Params>;
+}) {
+  const { categoryId } = await params;
 
-    const category = await getCategory(params.categoryId)
+  const [products, category] = await Promise.all([
+    getProducts({ categoryId }),
+    getCategory(categoryId),
+  ]);
 
-    return (
-        <div className="big-white">
-            <Container>
-                <Banner data={category.banner} />
-                <div className="px-4 sm:px-6 lg:px-8 pb-24">
-                    <div className="mt-6 lg:col-span-4 lg:mt-0">
-                        {products.length === 0 && <NoResults />}
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-                            {products.map((item) => (
-                                <ProductCard key={item.id} data={item} />
-                            ))}
+  return (
+    <div className="big-white">
+      <Container>
+        {/* Safety: kalau category/banner bisa null */}
+        {category?.banner && <Banner data={category.banner} />}
 
-                        </div>
-                    </div>
-                </div>
-            </Container>
-
+        <div className="px-4 sm:px-6 lg:px-8 pb-24">
+          <div className="mt-6 lg:col-span-4 lg:mt-0">
+            {products.length === 0 && <NoResults />}
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+              {products.map((item) => (
+                <ProductCard key={item.id} data={item} />
+              ))}
+            </div>
+          </div>
         </div>
-    )
+      </Container>
+    </div>
+  );
 }
-
-export default CategoryPage
